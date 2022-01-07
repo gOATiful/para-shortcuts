a363ec4b00d75cf183cbdf892b021498e31ab613
Torge <hinrichs.torge@gmail.com>
2022-01-07 15:14:21 +0100

    add command to move files to archive

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import { Editor, MarkdownView, Notice, Plugin } from 'obsidian';
+import {Notice, Plugin, TFolder } from 'obsidian';
 
 import { Settings, DEFAULT_SETTINGS } from 'settings/settings.js';
 import { ParaShortcutsSettingTab } from 'settings/settings_tab.js';
 import { CreateNewEntryModal } from 'modals/new_entry_modal.js';
+import { ParaType } from 'para_types';
+import { join } from 'path';
+import * as path from 'path/posix';
+import { existsSync } from 'fs';
 
 export default class ParaShortcutsPlugin extends Plugin {
 	settings: Settings;
@@ -22,18 +26,12 @@ export default class ParaShortcutsPlugin extends Plugin {
 		this.addCommand({
 			id: 'init-vault',
 			name: 'Init PARA folders',
-			callback: () => {
-				console.log("init");
-			}
+			callback: this.commandInitPara,
 		});
 		this.addCommand({
 			id: 'move-to-archive',
 			name: 'Move current file to archive',
-			callback: () => {
-				console.log("archive");
-				console.log(this.app.vault.getAllLoadedFiles());
-				console.log(this.app.vault.getRoot());
-			}
+			checkCallback: this.commandMoveToArchive,
 		});
 		this.addSettingTab(new ParaShortcutsSettingTab(this.app, this));
 	}
@@ -51,6 +49,57 @@ export default class ParaShortcutsPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+	}
+
+
+	private commandInitPara(){
+		console.log("init");
+	}
+
+	private commandMoveToArchive(checking: boolean): boolean | void {
+		let activeFile = this.app.workspace.getActiveFile();
+		if (checking){
+			return (activeFile === null) ? false : true;
+		}
+		if (activeFile !== null){
+			// do real stuff
+			let paraType = this.findParaTypeInPath(activeFile.parent);
+			if (paraType != ParaType.archive){
+				// move file to archive
+				let archiveFolderName = this.settings.folders.get(ParaType.archive);
+				let subfolderName = this.settings.folders.get(paraType);
+				let pathToFile = join(this.app.vault.getRoot().name, archiveFolderName, subfolderName, activeFile.name);
+				new Notice(`Moving file to: ${pathToFile}`);
+				this.app.fileManager.renameFile(activeFile, pathToFile).catch((err) => {
+					new Notice(`Unable to move file to: ${pathToFile}`);
+				});
+			}
+		}
+		return true;
+	}
+
+
+	/**
+	 * 
+	 * @param file 
+	 * @returns the ParaType if found, returns null if not
+	 */
+	private findParaTypeInPath(folder : TFolder) : ParaType | null {
+		var type : ParaType | null = null;
+		this.settings.folders.forEach((val, key)=> {
+			if (folder.name === val){
+				type = key;
+			}
+		});
+		if(!folder.isRoot()){ 
+			// always prioritize found type from parent to avoid nested folders
+			console.log("searching in parent");
+			let foundTypeInParent = this.findParaTypeInPath(folder.parent);
+			if (foundTypeInParent != null){
+				type = foundTypeInParent;
+			}
+		}
+		return type;
 	}
 }
 
